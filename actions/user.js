d007c63c5f040e46c3ac99e87d1c969ac180a092
SuyashJain17 <suayshjain1707@gmail.com>
2025-04-03 03:58:19 +0530

    added some other functionalities

--- actions/user.js
+++ actions/user.js
@@ -1,94 +1,93 @@
-"use server"
+"use server";
 
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { generateAIInsights } from "./dashboard";
 
 export async function updateUser(data) {
-    const {userId} = await auth();
-    if(!userId) throw new Error("Unauthorized");
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
 
-    const user = await db.user.findUnique({
-        where: {
-            clerkUserId: userId,
-        },
-    });
-    if(!user) throw new Error("User not found");
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
 
-    try {
-        const result = await db.$transaction(
-            async(tx) => {
-                let industryInsight = await tx.industryInsight.findUnique({
-                    where: {
-                        industries: data.industries,
-                    },
-                });
+  if (!user) throw new Error("User not found");
 
-                if(!industryInsight) {
-                    industryInsight = await tx.industryInsight.create({
-                        data: {
-                            industry: data.industry,
-                            salaryRanges: [],
-                            growthRate: 0,
-                            demandLevel: "Medium",
-                            topSkills: [],
-                            marketOutlook: "Neutral",
-                            keyTrends: [],
-                            recommendedSkills: [],
-                            nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-                        }
-                    });
-                }
-                const updateUser = await tx.user.update({
-                    where: {
-                        id: user.id,
-                    },
-                    data: {
-                        industry: data.industry,
-                        expierence: data.expierence,
-                        bio: data.bio,
-                        skills: data.skills,
-                    },
-                });
-                return {updateUser, industryInsight}
+  try {
+    const result = await db.$transaction(
+      async (tx) => {
+        let industryInsight = await tx.industryInsight.findUnique({
+          where: {
+            industry: data.industry,
+          },
+        });
+
+        if (!industryInsight) {
+          const insights = await generateAIInsights(data.industry);
+
+          industryInsight = await db.industryInsight.create({
+            data: {
+              industry: data.industry,
+              ...insights,
+              nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
             },
-            {
-                timeout: 10000 // defalt: 5000,
-            },
-        )
-        revalidatePath("/");
-        return result.user;
-        
-    } catch(error) {
-        console.error("Error updating user and industry:", error.message);
-        throw new Error("Failed to update profile");
-    }
+          });
+        }
+
+        const updatedUser = await tx.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            industry: data.industry,
+            experience: data.experience,
+            bio: data.bio,
+            skills: data.skills,
+          },
+        });
+
+        return { updatedUser, industryInsight };
+      },
+      {
+        timeout: 10000, 
+      }
+    );
+
+    revalidatePath("/");
+    return result.user;
+  } catch (error) {
+    console.error("Error updating user and industry:", error.message);
+    throw new Error("Failed to update profile");
+  }
 }
 
 export async function getUserOnboardingStatus() {
-    const {userId} = await auth();
-    if(!userId) throw new Error("Unauthorized");
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
 
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) throw new Error("User not found");
+
+  try {
     const user = await db.user.findUnique({
-        where: {
-            clerkUserId: userId,
-        },
+      where: {
+        clerkUserId: userId,
+      },
+      select: {
+        industry: true,
+      },
     });
-    if(!user) throw new Error("User not found");
 
-    try {
-        const user = await db.user.findUnique({
-            where: {
-                clerkUserId: userId,
-            },
-            select: {
-                industry: true,
-            }
-        });
-        return {
-            isOnboarding: !!user?.industry,
-        };
-    } catch(error) {
-        console.error("Error updating onboarding status:", error.message);
-        throw new Error("Failed to check onboarding status");
-    }
+    return {
+      isOnboarded: !!user?.industry,
+    };
+  } catch (error) {
+    console.error("Error checking onboarding status:", error);
+    throw new Error("Failed to check onboarding status");
+  }
 }
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,16 +1,21 @@
-import { getUserOnboardingStatus } from '@/actions/user';
-import { redirect } from 'next/navigation';
+import { getIndustryInsights } from "@/actions/dashboard";
+import { getUserOnboardingStatus } from "@/actions/user";
+import { redirect } from "next/navigation";
+import DashboardView from "./_components/dashboard-view";
 
-const IndustryInsightPage = async () => {
-const { isOnboarded } = await getUserOnboardingStatus();
-    
-    if(!isOnboarded) {
-        redirect("/onboarding");
-    }
+export default async function DashboardPage() {
+  const { isOnboarded } = await getUserOnboardingStatus();
 
-    return (
-    <div>IndustryInsightPage</div>
-)
+  
+  if (!isOnboarded) {
+    redirect("/onboarding");
+  }
+
+  const insights = await getIndustryInsights();
+
+  return (
+    <div className="container mx-auto">
+      <DashboardView insights={insights} />
+    </div>
+  );
 }
-
-export default IndustryInsightPage
