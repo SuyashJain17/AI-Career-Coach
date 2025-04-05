c5a974255c2fd900052ea3284894b9d934603e77
SuyashJain17 <suayshjain1707@gmail.com>
2025-04-05 14:09:54 +0530

    added ai-cover-letter feature and fixed some bugs for resume feature

--- app/lib/schema.js
+++ app/lib/schema.js
@@ -1,4 +1,3 @@
-import { ContactIcon } from "lucide-react";
 import { z } from "zod";
 
 export const onboardingSchema = z.object({
@@ -29,36 +28,45 @@ export const onboardingSchema = z.object({
 });
 
 export const contactSchema = z.object({
-  email:  z.string().email("Invalid email address"),
+  email: z.string().email("Invalid email address"),
   mobile: z.string().optional(),
   linkedin: z.string().optional(),
   twitter: z.string().optional(),
-})
+});
 
-export const entrySchema = z.object({
-  title: z.string().min(1, "Title is required"),
-  organization: z.string().min(1, "Organization is required"),
-  startDate: z.string().min(1, "start date is required"),
-  endDate: z.string().optional(),
-  description: z.string().min(1, "Description is required"),
-  current: z.boolean().default(false),
-}).refine(
-  (data) => {
-    if(!data.current && !data.endDate) {
-      return false;
+export const entrySchema = z
+  .object({
+    title: z.string().min(1, "Title is required"),
+    organization: z.string().min(1, "Organization is required"),
+    startDate: z.string().min(1, "Start date is required"),
+    endDate: z.string().optional(),
+    description: z.string().min(1, "Description is required"),
+    current: z.boolean().default(false),
+  })
+  .refine(
+    (data) => {
+      if (!data.current && !data.endDate) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: "End date is required unless this is your current position",
+      path: ["endDate"],
     }
-    return true;
-  }, {
-    message: "End date is required useless this is your current position",
-    path: ["endDate"],  
-  }
-)
+  );
 
 export const resumeSchema = z.object({
-  ContactInfo: contactSchema,
-  summary: z.string().min(1, "Proffesional summary is required"),
-  skills: z.string().min(1, "skills are required"),
-  expierence: z.array(entrySchema),
+  contactInfo: contactSchema,
+  summary: z.string().min(1, "Professional summary is required"),
+  skills: z.string().min(1, "Skills are required"),
+  experience: z.array(entrySchema),
   education: z.array(entrySchema),
   projects: z.array(entrySchema),
-})
+});
+
+export const coverLetterSchema = z.object({
+  companyName: z.string().min(1, "Company name is required"),
+  jobTitle: z.string().min(1, "Job title is required"),
+  jobDescription: z.string().min(1, "Job description is required"),
+});
